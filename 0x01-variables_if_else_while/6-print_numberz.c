5762f8b15c1296dcdb60c64ff2128c7976fd445a
vIdzapurity254 <purityaligula@gmail.com>
2023-02-17 13:59:00 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,15 +5,14 @@
  * followed by a new line, using putchar
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = 48; n < 58; n++)
-        {
-                putchar(n);
-        }
-        putchar('\n');
-        return (0);
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
 }
