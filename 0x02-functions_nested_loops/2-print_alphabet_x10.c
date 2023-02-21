f819e234e7022affbaeb2ccfe271977ad0769145
vIdzapurity254 <purityaligula@gmail.com>
2023-02-21 14:23:18 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,7 @@ void print_alphabet_x10(void)
 	for (ten = 0; ten <= 9; ten++)
 	{
 		for (la = 'a'; la <= 'z'; la++)
-			_putcher(la);
-		_putcher('\n');
+			_putchar(la);
+		_purchar('\n');
 	}
 }
-
