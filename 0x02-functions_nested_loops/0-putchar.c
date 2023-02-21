38efb3772fd6226895f85ecd07725fa830229a6b
vIdzapurity254 <purityaligula@gmail.com>
2023-02-21 06:59:48 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -prints _putchar and a new line
+ * Return:Always 0
+ */
+int main (void)
+{
+	_Putchar('-')
+	_Putchar('p')
+	_Putchar('u');
+	_Putchar('t');
+	_putchar('c');
+	_Putchar('h');
+	_Putchar('a');
+	_Putchar('r');
+	_Putchar('/n')
+
+	return (0);
+}
+
