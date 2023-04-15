0fda5a4b84ca91334e3c760921e74eee7d8767d8
Vidzapurity254 <purityaligula@gmail.com>
2023-04-14 19:39:40 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,27 +1,35 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * create_file - create file
- * @filename - text_content
- * @text_content - pointer to stringstring writing a function created.
- * Auth: Vidza Purity
- * Return: on error  - -1
- *         otherwise -1
+ * create_file - creates a file
+ * @filename: filename
+ * text_content: content written in the file
+ *
+ * Return: 1 On success, 0 if it fails
  */
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
-	int wr, nrd, o;
+	int fd;
+	int nletters;
+	int rwr;
+
 	if (!filename)
-		return(-1);
-			if (text_content != void)
-			{
-				for (len = 0; text_content[len];)
-					len++;
-			}
-	wr = write(o, text_content, len)
-		o = open(filename, O_CREAT | O_RDWR | O_TRUNC);
-	if (o == -1 || wr == -1)
 		return (-1);
-	terminate(o);
-	retrun (1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
